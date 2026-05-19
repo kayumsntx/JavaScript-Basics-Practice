0bc8acd43c948eed2f5f2cc097e058366367c41c
kayumsntx <mdcu2024@gmail.com>
2026-05-19 07:12:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+
+var friendsName="John";
+console.log(friendsName);
+
+var strType="This is a string";
+var numType=123;
+var boolType=true;
+console.log(strType);
+console.log(numType);
+console.log(boolType); 
+
+var paragraphUpper="This is a paragraph. It can contain multiple sentences. It is used to group related sentences together.";
+console.log(paragraphUpper.toUpperCase());
+
+var paragraphLower="This is a paragraph. It can contain multiple sentences. It is used to group related sentences together.";
+console.log(paragraphLower.toLowerCase());
+
+var myFd = "John, Doe, Jane, Smith";
+myFd.split("John");
+console.log(myFd.split("John"));
+
+var myFd = "John Doe Jane Smith";
+console.log(myFd.indexOf("S"));
